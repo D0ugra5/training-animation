8f0bcfcd0a355db8a4b00b83de668bba065ed54d
Douglas Sousa <douglasmb87@gmail.com>
2023-08-07 14:54:50 -0300

    feat: add animation for scroll

---
+++ js/Cards.js
@@ -0,0 +1,27 @@
+class Cards {
+  constructor() {
+    this.getPositionTips = document.getElementById("section-dicas");
+    this.getCardTipsEsq = document.querySelector(".card-esq");
+    this.getCardTipsDir = document.querySelector(".card-dir");
+  }
+  teste() {
+    console.log("scroll");
+  }
+
+  scrollCards() {
+    window.requestAnimationFrame(this.calculoScroll.bind(this));
+  }
+
+  calculoScroll() {
+    const posicao = this.getPositionTips.getBoundingClientRect()["y"];
+
+    console.log(posicao);
+    if (posicao >= 25) {
+      // let newPosition = 25 - Math.log(posicao) * 3;
+      this.getCardTipsEsq.style.transform = `translate(${-posicao + 25}%)`;
+      this.getCardTipsDir.style.transform = `translate(${posicao - 25}%)`;
+    }
+  }
+}
+
+export { Cards };
---
+++ js/script.js
@@ -0,0 +1,6 @@
+import { Cards } from "./cards.js";
+
+window.onload = () => {
+  const animaCards = new Cards();
+  document.addEventListener("scroll", animaCards.scrollCards.bind(animaCards));
+};
